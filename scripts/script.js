b5299ef68c3447ae20c0fc3800b855577a46778e
Arthur Vershyhora <ArturiTunes@gmail.com>
2018-10-30 08:07:18 +0200

    Added chackup value user's input not empty

--- scripts/script.js
+++ scripts/script.js
@@ -3,11 +3,14 @@ let main = function (event) {
 	let btn = document.querySelector(".comment-input button");
 	btn.addEventListener("click", function(){
 		let userComment = document.querySelector(".comment-input input").value;
-		let newP = document.createElement("p");
-		let newContent = document.createTextNode(userComment);
-		newP.appendChild(newContent);
-		let commentsSection = document.querySelector(".comments");
-		commentsSection.appendChild(newP);
+		if (userComment !== "") {
+			let newP = document.createElement("p");
+			let newContent = document.createTextNode(userComment);
+			newP.appendChild(newContent);
+			let commentsSection = document.querySelector(".comments");
+			commentsSection.appendChild(newP);
+		};
+		
 
 	}, false);
 	
