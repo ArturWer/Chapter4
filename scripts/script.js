2d68c9274b06ae6befdcbe1da9540ae76f5cc887
Arthur Vershyhora <ArturiTunes@gmail.com>
2018-10-30 10:16:40 +0200

    Added keypress Enter button user input and added 2 function

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,31 @@
 let main = function (event) {
 	'use striсt';
+	let userInput = document.querySelector(".comment-input input");
 	let btn = document.querySelector(".comment-input button");
-	btn.addEventListener("click", function(){
-		let userComment = document.querySelector(".comment-input input").value;
-		if (userComment !== "") {
-			let newP = document.createElement("p");
-			let newContent = document.createTextNode(userComment);
-			newP.appendChild(newContent);
-			let commentsSection = document.querySelector(".comments");
-			commentsSection.appendChild(newP);
-			document.querySelector(".comment-input input").value = "";
-		};
-		
 
-	}, false);
+	function addNewComment(){		
+		let userComment = userInput.value;
+		let newP = document.createElement("p");
+		let newContent = document.createTextNode(userComment);
+		newP.appendChild(newContent);
+		let commentsSection = document.querySelector(".comments");
+		commentsSection.appendChild(newP);
+		userInput.value = "";
+	};
+	function checkNewComment(){
+		if (userInput.value !== "") {
+			addNewComment();
+		};
+	};
+
+	btn.addEventListener("click", checkNewComment, false);
 	
+	userInput.addEventListener("keypress", function(event){
+		if (event.keyCode === 13) {
+			console.log("Enter");
+			checkNewComment();
+		}
+	}, false);
+
 };
 $(document).ready(main);
