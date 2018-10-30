d33b9e15b2fca8ca7e13aa777ccff3f753ddbf3d
Arthur Vershyhora <ArturiTunes@gmail.com>
2018-10-30 06:10:26 +0200

    Added js code (function main and addEventListener for button)

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,9 @@
-'use striсt';
+let main = function (event) {
+	'use striсt';
+	let btn = document.querySelector('.comment-input button');
+	btn.addEventListener('click', function(){
+		console.log(btn);
+	}, false);
+	
+};
+$(document).ready(main);
