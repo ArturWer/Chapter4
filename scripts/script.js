ec7e599fcada826c6a499bac6bfb709d6c1b8c27
Arthur Vershyhora <ArturiTunes@gmail.com>
2018-10-30 07:15:49 +0200

    Added creating new comment's elemend and adding this element to comment section

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,14 @@
 let main = function (event) {
 	'use striсt';
-	let btn = document.querySelector('.comment-input button');
+	let btn = document.querySelector(".comment-input button");
 	btn.addEventListener('click', function(){
-		console.log(btn);
+		let msg = "It's a new comment";
+		let newP = document.createElement("p");
+		let newContent = document.createTextNode(msg);
+		newP.appendChild(newContent);
+		let commentsSection = document.querySelector(".comments");
+		commentsSection.appendChild(newP);
+
 	}, false);
 	
 };
